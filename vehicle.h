ed9187e1b54cd7158e19115ff5c478270eb97eb4
Hongjunyeong <105625862+gaenyangi@users.noreply.github.com>
2024-06-05 22:41:13 -0700

    Add files via upload

--- vehicle.h
+++ vehicle.h
@@ -2,6 +2,7 @@
 #define __PROJECTS_PROJECT2_VEHICLE_H__
 
 #include "projects/crossroads/position.h"
+#include "threads/synch.h"
 
 #define VEHICLE_STATUS_READY 	0
 #define VEHICLE_STATUS_RUNNING	1
@@ -14,8 +15,10 @@ struct vehicle_info {
 	char dest;
 	struct position position;
 	struct lock **map_locks;
+    bool in_critical_area; 
 };
 
 void vehicle_loop(void *vi);
+void init_on_mainthread(int thread_cnt);
 
 #endif /* __PROJECTS_PROJECT2_VEHICLE_H__ */
